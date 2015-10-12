ba2ef2386737792f95e0d8f3d33afe74e10974d5
Jesse Hoobergs <jesse@hoobergs.be>
2015-10-12 15:35:49 +0200

    HomeAcitivity

---
+++ NightGame/app/src/main/java/be/cwa3/nightgame/HomeActivity.java
@@ -0,0 +1,16 @@
+package be.cwa3.nightgame;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jesse on 12/10/2015.
+ */
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
