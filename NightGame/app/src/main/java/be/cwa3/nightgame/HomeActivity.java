302c51bee8f211a2fb3d2eb027a5c803bab6c4e1
Jesse Hoobergs <jesse@hoobergs.be>
2015-10-12 15:57:39 +0200

    Eerste map werkend

--- NightGame/app/src/main/java/be/cwa3/nightgame/HomeActivity.java
+++ NightGame/app/src/main/java/be/cwa3/nightgame/HomeActivity.java
@@ -1,16 +1,28 @@
 package be.cwa3.nightgame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by jesse on 12/10/2015.
  */
 public class HomeActivity extends AppCompatActivity {
+    Button buttonPlay;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        buttonPlay = (Button) findViewById(R.id.button_play);
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
