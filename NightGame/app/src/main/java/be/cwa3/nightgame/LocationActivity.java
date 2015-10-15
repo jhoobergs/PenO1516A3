880d522f2179ad0cef167f99292ce6ca8e2a70b8
Jesse Hoobergs <jesse@hoobergs.be>
2015-10-15 17:38:26 +0200

    Right location for map in locations

--- NightGame/app/src/main/java/be/cwa3/nightgame/LocationActivity.java
+++ NightGame/app/src/main/java/be/cwa3/nightgame/LocationActivity.java
@@ -12,6 +12,12 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -19,24 +25,42 @@ import java.util.Date;
 /**
  * Created by jesse on 15/10/2015.
  */
-public class LocationActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
-    private TextView textViewLocation;
+public class LocationActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, OnMapReadyCallback {
     private boolean mRequestingLocationUpdates = true;
     private Location mCurrentLocation;
     private String mLastUpdateTime;
     private GoogleApiClient mGoogleApiClient;
     LocationRequest mLocationRequest;
+    MapFragment mapFragment;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_location);
-        textViewLocation = (TextView) findViewById(R.id.textViewLocation);
+        setContentView(R.layout.activity_map);
+        mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
         createLocationRequest();
         buildGoogleApiClient();
         if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates) {
             startLocationUpdates();
         }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        if(mCurrentLocation != null)
+            sydney = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -76,7 +100,7 @@ public class LocationActivity extends AppCompatActivity implements GoogleApiClie
     public void onLocationChanged(Location location) {
         mCurrentLocation = location;
         mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
-        textViewLocation.setText(String.format("%s %s", location.getLatitude(), location.getLongitude()));
+        mapFragment.getMapAsync(this);
     }
 
     @Override
