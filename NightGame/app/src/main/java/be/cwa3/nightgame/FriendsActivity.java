6d3a000c1e46e3585c0f1068dfac90a7047f3b67
kvnzhng <kevinzhang230@hotmail.com>
2015-10-19 16:37:50 +0200

    Friends
    Add button added
    Friends Adapter improved

--- NightGame/app/src/main/java/be/cwa3/nightgame/FriendsActivity.java
+++ NightGame/app/src/main/java/be/cwa3/nightgame/FriendsActivity.java
@@ -2,11 +2,15 @@ package be.cwa3.nightgame;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import be.cwa3.nightgame.Adapters.FriendsAdapter;
 
 /**
  * Created by kevin on 19/10/2015.
@@ -50,7 +54,23 @@ public class FriendsActivity extends AppCompatActivity {
         fakeData.add("test1");
         fakeData.add("test1");
 
-        listView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,android.R.id.text1,fakeData)
-        );
+        listView.setAdapter(new FriendsAdapter(this, fakeData));
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.friends_activity, menu);
+            return super.onCreateOptionsMenu(menu);
+        }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+            }
+        }
+
 }
