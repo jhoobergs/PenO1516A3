f3cc10cd6547607161e98e6437bcd404aa206796
koengeerts <Koen.geerts8@hotmail.com>
2015-10-19 15:14:13 +0200

    1

---
+++ NightGame/app/src/main/java/be/cwa3/nightgame/PlayActivity.java
@@ -0,0 +1,14 @@
+package be.cwa3.nightgame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Koen on 19/10/2015.
+ */
+public class PlayActivity extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
