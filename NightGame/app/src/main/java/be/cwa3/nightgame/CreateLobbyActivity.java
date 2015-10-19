78cca52b52f70c57ec692b2acdb2205d9e856e7c
MoranGybels <mooke_moran@hotmail.com>
2015-10-19 17:46:03 +0200

    CreateLobby(probeersel1)

---
+++ NightGame/app/src/main/java/be/cwa3/nightgame/CreateLobbyActivity.java
@@ -0,0 +1,15 @@
+package be.cwa3.nightgame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Gebruiker on 19/10/2015.
+ */
+public class CreateLobbyActivity extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createlobby);
+    }
+}
--- NightGame/app/src/main/java/be/cwa3/nightgame/PlayActivity.java
+++ NightGame/app/src/main/java/be/cwa3/nightgame/PlayActivity.java
@@ -1,20 +1,32 @@
 package be.cwa3.nightgame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 /**
  * Created by Koen on 19/10/2015.
  */
 public class PlayActivity extends AppCompatActivity {
-    String[] myStringArray={"A","B","C","D"};
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        String[] myStringArray={"A","B","C","D","E","F","G","H","J","K","L","M","N","O","P","Q","R","S","T","U","V","W"};
+        ArrayAdapter<String> myAdapter=new
+                ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                myStringArray);
+        ListView myList=
+                (ListView) findViewById(R.id.listView);
+        myList.setAdapter(myAdapter);
 
     }
 
@@ -28,6 +40,8 @@ public class PlayActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.add:
+                Intent intent = new Intent(getApplicationContext(), CreateLobbyActivity.class);
+                startActivity(intent);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
