cd143dd2fb948fd76f167accf8049a0952841ee5
Jesse Hoobergs <jesse@hoobergs.be>
2015-10-24 18:01:15 +0200

    servercode verbetert en in meerdere files gesplitst

---
+++ ServerCode/scoreboard.js
@@ -0,0 +1,20 @@
+exports = module.exports = function(app){
+
+app.get('/scoreboard/list', function(req, res){
+    res.send({'List' : [
+              { 'Name' : 'Jesse', 'ImageURL' : 'http://www.benveldkamp.nl/images/PERS/Smurfen-bril.jpg', 'Wins' : 5, 'Games' : 7, 'Missions' : 10},
+              { 'Name' : 'Jean', 'ImageURL' : 'http://www.benveldkamp.nl/images/PERS/Smurfen-bril.jpg', 'Wins' : 0, 'Games' : 4, 'Missions' : 3},
+              { 'Name' : 'Koen', 'ImageURL' : 'http://www.benveldkamp.nl/images/PERS/Smurfen-bril.jpg', 'Wins' : 2, 'Games' : 9, 'Missions' : 12},
+              { 'Name' : 'Kevin', 'ImageURL' : 'http://www.benveldkamp.nl/images/PERS/Smurfen-bril.jpg', 'Wins' : 3, 'Games' : 5, 'Missions' : 8},
+              { 'Name' : 'Moran', 'ImageURL' : 'http://www.benveldkamp.nl/images/PERS/Smurfen-bril.jpg', 'Wins' : 5, 'Games' : 9, 'Missions' : 11},
+              { 'Name' : 'Elisabeth', 'ImageURL' : 'http://www.benveldkamp.nl/images/PERS/Smurfen-bril.jpg', 'Wins' : 4, 'Games' : 5, 'Missions' : 3},         { 'Name' : 'Jesse', 'ImageURL' : 'http://www.benveldkamp.nl/images/PERS/Smurfen-bril.jpg', 'Wins' : 5, 'Games' : 7, 'Missions' : 10},
+              { 'Name' : 'Jean', 'ImageURL' : 'http://www.benveldkamp.nl/images/PERS/Smurfen-bril.jpg', 'Wins' : 0, 'Games' : 4, 'Missions' : 3},
+              { 'Name' : 'Koen', 'ImageURL' : 'http://www.benveldkamp.nl/images/PERS/Smurfen-bril.jpg', 'Wins' : 2, 'Games' : 9, 'Missions' : 12},
+              { 'Name' : 'Kevin', 'ImageURL' : 'http://www.benveldkamp.nl/images/PERS/Smurfen-bril.jpg', 'Wins' : 3, 'Games' : 5, 'Missions' : 8},
+              { 'Name' : 'Moran', 'ImageURL' : 'http://www.benveldkamp.nl/images/PERS/Smurfen-bril.jpg', 'Wins' : 5, 'Games' : 9, 'Missions' : 11},
+              { 'Name' : 'Elisabeth', 'ImageURL' : 'http://www.benveldkamp.nl/images/PERS/Smurfen-bril.jpg', 'Wins' : 4, 'Games' : 5, 'Missions' : 3}
+          
+    ]});
+});
+
+}
---
+++ ServerCode/user.js
@@ -0,0 +1,169 @@
+exports = module.exports = function(app, AWS){
+
+app.post('/user/login', function(req, res) {
+    var dynamodbDoc = new AWS.DynamoDB.DocumentClient();
+console.log(req.body);
+var error = "";
+if(req.body != null){
+    console.log("Querying for Users with id.");
+    if(req.body.Username == null ||req.body.Password == null){
+        res.send("{Not all params present}");
+    }
+    else{
+        var params = {
+            TableName : "Users",
+            KeyConditionExpression: "#username = :name",
+            ExpressionAttributeNames:{
+                "#username": "Username"
+            },
+            ExpressionAttributeValues: {
+                ":name":req.body.Username
+            }
+        };
+    
+
+        dynamodbDoc.query(params, function(err, data) {
+            if (err) {
+                console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+            } else {
+                console.log("Logging in");
+                if(data.Items.length == 1){
+                    if(bcrypt.compareSync(req.body.Password, data.Items[0].Password)){
+                            createToken(data.Items[0].Username, function(token) {
+                            var result = {
+                                'Username' : data.Items[0].Username,
+                                'Token' : token
+                            };
+                            res.send(result)
+                        });                    
+                    
+                    }                
+                    else{
+                        res.send("{Wrong Password}");
+                    }
+                }
+                else{
+                        res.send("{User Not found}");
+                    }
+            }
+        });
+    }
+}
+else{
+    res.send("{No body}");
+}  
+});
+
+app.post('/user/create', function(req, res) {
+    var dynamodbDoc = new AWS.DynamoDB.DocumentClient();
+console.log(req.body);
+var error = "";
+if(req.body != null){
+    console.log("Creating for Users with id.");
+    if(req.body.Username == null ||req.body.Password == null || req.body.Password != req.body.PasswordRepeat || req.body.Email == null){
+        res.send("{Not all params present}");
+    }
+    else{
+        var params = {
+            TableName : "Users",
+            KeyConditionExpression: "#username = :name",
+            ExpressionAttributeNames:{
+                "#username": "Username"
+            },
+            ExpressionAttributeValues: {
+                ":name":req.body.Username
+            }
+        };    
+
+        dynamodbDoc.query(params, function(err, data) {
+            if (err) {
+                console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+            } else {
+                console.log("Query succeeded.");
+                if(data.Items.length == 0){
+                    //Ok, we can create the user                            
+                    putnewUserItem(req.body);
+                    createToken(req.body.Username, function(token) {
+                        var result = {
+                            'Username' : req.body.Username,
+                            'Token' : token
+                        };
+                        returnData(res, 1,result, null);
+                    });                    
+                }
+                else{
+                returnData(res, 0,{}, "Username does allready exist.");
+                }
+            }
+        });
+    }
+}
+else{
+    res.send("{No body}");
+}  
+});
+
+putnewUserItem = function(data) {           
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(data.Password, salt);
+    var tableName = 'Users';
+    var item = {
+	    'Username' : { 'S': data.Username },
+	    'Password' : { 'S' : hash},
+        'Email' : { 'S' : data.Email}
+    };	
+    dd.putItem({
+        'TableName': tableName,
+        'Item': item
+    }, function(err, data) {
+        err && console.log(err);
+    });
+};
+
+putnewTokenItem = function(Username, Token) {   
+    var tableName = 'Tokens';
+    var item = {
+	    'Token' : { 'S': Token },
+	    'Username' : { 'S' : Username}
+    };	
+    dd.putItem({
+        'TableName': tableName,
+        'Item': item
+    }, function(err, data) {
+        err && console.log(err);
+    });
+};
+
+function createToken(Username, callback){
+    var dynamodbDoc = new AWS.DynamoDB.DocumentClient();
+    var salt = bcrypt.genSaltSync(10);
+                    var token = bcrypt.hashSync(salt, salt); //Just some randomness
+                    
+                    var checkIfTokenExistsParams = {
+                            TableName : "Tokens",
+                            KeyConditionExpression: "#token = :name",
+                            ExpressionAttributeNames:{
+                            "#token": "Token"
+                        },
+                        ExpressionAttributeValues: {
+                            ":name":token
+                        }                    
+                    };
+                    
+                    dynamodbDoc.query(checkIfTokenExistsParams, function(err, data) {
+                        if (err) {
+                            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+                        } else {
+                            if(data.Items.length  == 0){
+                                putnewTokenItem(Username, token);
+                                callback(token);
+                            }
+                            else{
+                                createToken(Username);
+                            }
+                        }
+                    });
+    
+}
+
+}
