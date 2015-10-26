12f37f66adfaf85c41c0c9c5b8f954e84073bb5d
Jesse Hoobergs <jesse@hoobergs.be>
2015-10-26 17:02:31 +0100

    Eerste pagina teruggezet naar Login

--- NightGame/app/src/main/java/be/cwa3/nightgame/SplashActivity.java
+++ NightGame/app/src/main/java/be/cwa3/nightgame/SplashActivity.java
@@ -27,8 +27,7 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                //Intent i = new Intent(getApplicationContext(), LoginActivity.class);
-                Intent i = new Intent(getApplicationContext(), SoundActivity.class);
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(i);
 
                 // close this activity
