35612a86fb1726bef4c0a4c3cf230f1519bfc4b7
koengeerts <Koen.geerts8@hotmail.com>
2015-11-05 16:47:47 +0100

    accelerometer

--- NightGame/app/src/main/java/be/cwa3/nightgame/SplashActivity.java
+++ NightGame/app/src/main/java/be/cwa3/nightgame/SplashActivity.java
@@ -27,7 +27,7 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                Intent i = new Intent(getApplicationContext(), AccelerometerActivity.class);
                 startActivity(i);
 
                 // close this activity
