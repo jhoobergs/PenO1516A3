60220e91dd5ccb5035d99f51a00d859837053aaf
koengeerts <Koen.geerts8@hotmail.com>
2015-11-05 18:36:55 +0100

    knopjes scorebord

--- NightGame/app/src/main/java/be/cwa3/nightgame/SplashActivity.java
+++ NightGame/app/src/main/java/be/cwa3/nightgame/SplashActivity.java
@@ -27,7 +27,7 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                Intent i = new Intent(getApplicationContext(), HomeActivity.class);
                 startActivity(i);
 
                 // close this activity
