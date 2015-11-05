582a270c52435088da3a48703baa059740c5f5ea
Jesse Hoobergs <jesse@hoobergs.be>
2015-11-05 16:04:21 +0100

    Sound activity accesible from homeActivity menu

--- NightGame/app/src/main/java/be/cwa3/nightgame/HomeActivity.java
+++ NightGame/app/src/main/java/be/cwa3/nightgame/HomeActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -67,8 +69,26 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sound:
+                Intent intent = new Intent(getApplicationContext(), SoundActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         buttonPlay.setEnabled(true);
         buttonFriend.setEnabled(true);
         buttonScoreBoard.setEnabled(true);
