b5ecb14d240c2b0ac3e555a8bf08967a5d994b47
koengeerts <Koen.geerts8@hotmail.com>
2015-11-09 17:30:10 +0100

    vies pro changes

--- NightGame/app/src/main/java/be/cwa3/nightgame/ScoreboardActivity.java
+++ NightGame/app/src/main/java/be/cwa3/nightgame/ScoreboardActivity.java
@@ -1,67 +1,128 @@
 package be.cwa3.nightgame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.Collections;
+import java.util.Comparator;
+
 import be.cwa3.nightgame.Adapters.ScoreboardAdapter;
+import be.cwa3.nightgame.Data.ErrorData;
+import be.cwa3.nightgame.Data.LoginReturnData;
 import be.cwa3.nightgame.Data.ReturnData;
+import be.cwa3.nightgame.Data.ScoreboardData;
 import be.cwa3.nightgame.Data.ScoreboardListData;
 import be.cwa3.nightgame.Utils.ApiUtil;
+import be.cwa3.nightgame.Utils.ErrorUtil;
+import be.cwa3.nightgame.Utils.RequestInterface;
+import be.cwa3.nightgame.Utils.RequestUtil;
 import retrofit.Call;
 import retrofit.Callback;
 import retrofit.Response;
 import retrofit.Retrofit;
 
 /**
- * Created by kevin on 19/10/2015.
+ * Improved/Upgraded/Enhanced by Koen en Jean on 9/11/2015.
  */
 public class ScoreboardActivity extends AppCompatActivity {
     private ScoreboardListData scoreboardListData;
     ListView listView;
+    Button buttonGames, buttonWins, buttonMissions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoreboard);
-
         makeCall();
-
         listView = (ListView)findViewById(R.id.listViewScores);
 
+        buttonGames = (Button) findViewById(R.id.button_games);
+        buttonMissions = (Button) findViewById(R.id.button_missions);
+        buttonWins = (Button) findViewById(R.id.button_wins);
+
+        buttonGames.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonGames.setEnabled(false);
+                buttonMissions.setEnabled(true);
+                buttonWins.setEnabled(true);
+                Collections.sort(scoreboardListData.List, new Comparator<ScoreboardData>() {
+                    @Override
+                    public int compare(ScoreboardData lhs, ScoreboardData rhs) {
+                        return isBetter(rhs.Games, lhs.Games, rhs.Wins, lhs.Wins, rhs.Missions, lhs.Missions);
+                    }
+                });
+                setListView();
+            }
+        });
+        buttonWins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonGames.setEnabled(true);
+                buttonMissions.setEnabled(true);
+                buttonWins.setEnabled(false);
+                Collections.sort(scoreboardListData.List, new Comparator<ScoreboardData>() {
+                    @Override
+                    public int compare(ScoreboardData lhs, ScoreboardData rhs) {
+                        return isBetter(rhs.Wins, lhs.Wins, lhs.Games, rhs.Games, rhs.Missions, lhs.Missions); //Games omgewisseld omdat minder gespeelde games bij hetzelfde aantal wins beter is
+                    }
+                });
+                setListView();
+            }
+        });
+        buttonMissions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonGames.setEnabled(true);
+                buttonMissions.setEnabled(false);
+                buttonWins.setEnabled(true);
+                Collections.sort(scoreboardListData.List, new Comparator<ScoreboardData>() {
+                    @Override
+                    public int compare(ScoreboardData lhs, ScoreboardData rhs) {
+                        return isBetter(rhs.Missions, lhs.Missions, rhs.Games, lhs.Games, rhs.Wins, lhs.Wins);
+                    }
+                });
+                setListView();
+            }
+        });
     }
     private void makeCall(){
         Call<ReturnData<ScoreboardListData>> call =
                 new ApiUtil().getApiInterface(this).loadScoreboard();
-        call.enqueue(new Callback<ReturnData<ScoreboardListData>>() {
+        final RequestUtil<ScoreboardListData> requestUtil = new RequestUtil<>(this, call);
+        requestUtil.makeRequest(new RequestInterface<ScoreboardListData>() {
             @Override
-            public void onResponse(Response<ReturnData<ScoreboardListData>> response, Retrofit retrofit) {
-                if (response.isSuccess()) {
-                    if(response.body().statusCode == 1) {
-                        scoreboardListData = response.body().body;
-                        setListView();
+            public void onSucces(ScoreboardListData body) {
+                scoreboardListData = body;
+                Collections.sort(scoreboardListData.List, new Comparator<ScoreboardData>() {
+                    @Override
+                    public int compare(ScoreboardData lhs, ScoreboardData rhs) {
+                        return isBetter(rhs.Games, lhs.Games, rhs.Wins, lhs.Wins, rhs.Missions, lhs.Missions);
                     }
-                    else{
-                        //Should not happen
-                        Toast.makeText(getApplicationContext(), "Should not happen", Toast.LENGTH_SHORT).show();
-                    }
-                } else
-                    Toast.makeText(getApplicationContext(), "No succes", Toast.LENGTH_SHORT).show();
+                });
+                buttonGames.setEnabled(false);
+                setListView();
             }
 
             @Override
-            public void onFailure(Throwable t) {
-                Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_SHORT).show();
+            public void onError(ErrorData error) {
+                Toast.makeText(getApplicationContext(), ErrorUtil.getErrorText(getApplicationContext(), error.Errors), Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     private void setListView(){
         if(scoreboardListData != null) {
+            for (ScoreboardData scoreboardData : scoreboardListData.List){
+                scoreboardData.isOpen = false;
+            }
             listView.setAdapter(new ScoreboardAdapter(ScoreboardActivity.this, scoreboardListData.List));
             listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
@@ -74,6 +135,27 @@ public class ScoreboardActivity extends AppCompatActivity {
                         linearLayoutextraData.setVisibility(View.GONE);
                 }
             });
+        }
+    }
+
+    public int isBetter(int rhs1, int lhs1, int rhs2, int lhs2, int rhs3, int lhs3) {
+        if (rhs1 > lhs1) {
+            return 1;
+        }
+        else if (lhs1 > rhs1) {
+            return -1;
+        }
+        if (rhs2 > lhs2) {
+            return 1;
+        }
+        else if (lhs2 > rhs2) {
+            return -1;
+        }
+        if (rhs3 > lhs3) {
+            return 1;
+        }
+        else {
+            return -1;
         }
     }
 }
