adc373ef1c0bc5b8edbeddec47e67fddee109ada
Jesse Hoobergs <jesse@hoobergs.be>
2015-11-18 10:10:05 +0100

    ServerCode - Vrienden volledig werkend met Accepted en IsSender parameters - Game: SendData werkend en bugfix voor de game/join - Server: Functies om afstand te berekenen toegevoegd. (Vincenty algoritme zoals op android)

--- ServerCode/friends.js
+++ ServerCode/friends.js
@@ -10,13 +10,24 @@ app.get('/friends/list', function(req, res){
                 var result = [];
                 var number = 0;
                 if(data.Items[0].Friends != null){
-                    //console.log(data.Items[0].Friends);
+                    if(Object.keys(data.Items[0].Friends).length == 0){
+                        returnData(res, 1, {'List' : result}, null);
+                    }
                     for (var i in data.Items[0].Friends) {
-                        //console.log(i);
-                        getUser(data.Items[0].Friends[i].Name, function(friendData){
+                        getUser(i, function(friendData){
+                            if(friendData != null){
                             if(friendData.Items[0].ImageURL == null)
                                 friendData.Items[0].ImageURL = 'http://www.benveldkamp.nl/images/PERS/Smurfen-bril.jpg';
-                            result.push({"Name" : friendData.Items[0].Username, "ImageURL": friendData.Items[0].ImageURL});
+                                console.log(data.Items[0].Friends[friendData.Items[0].Username]);
+                                console.log(friendData.Items[0].Username);
+                            result.push(
+                                {
+                                  "Name" : friendData.Items[0].Username,
+                                  "ImageURL": friendData.Items[0].ImageURL,
+                                  "Accepted" : data.Items[0].Friends[friendData.Items[0].Username].Accepted,
+                                  "IsSender": data.Items[0].Friends[friendData.Items[0].Username].IsSender
+                                });
+                            }
                             number++;
                             if(number == Object.keys(data.Items[0].Friends).length){
                                 returnData(res, 1, {'List' : result}, null);
@@ -98,17 +109,26 @@ app.post('/friends/add',  function(req, res){
                         if(data != null){
                         var friends = data.Items[0].Friends;
                         var found = false;
+                        var accepted = false;
+                        var isSender= true; //Dit betekent dat de gebruik die data krijgt de verstuurder is.
                         for (var i in friends) {
-                            if(friends[i] == req.body.Username){
+                            if(i == req.body.Username){
                                 found = true;
+                                accepted = friends[i].Accepted;
+                                isSender = friends[i].IsSender;
                                 break;
                             }
-                        }                            
-                        if(!found){
+                        }
+
+                        if((!found || (found && !accepted && !isSender)) && !(user == req.body.Username)){
                             console.log("adding");
-                            addFriendToUser(req.body.Username, user, function(succes){
+                            if(found){
+                                accepted = true;
+                            }
+                                
+                            addFriendToUser(!isSender, accepted, req.body.Username, user, function(succes){
                                 if(succes){
-                                    addFriendToUser(user, req.body.Username, function(succes){
+                                    addFriendToUser(isSender, accepted, user, req.body.Username, function(succes){
                                     if(succes){
                                         var result = {};
                                         returnData(res, 1, result, null);
@@ -120,6 +140,61 @@ app.post('/friends/add',  function(req, res){
                                 }
                                 else{
                                     returnData(res, 0, null, '{UpdateItemError}');
+                                }
+                            });
+                            
+                        }
+                        else{
+                            var result = {};
+                            returnData(res, 1, result, null);
+                        }
+                        }
+                        
+                    });
+                }
+            });
+        }
+    }
+
+});
+    
+    
+app.post('/friends/remove',  function(req, res){
+    var dynamodbDoc = new AWS.DynamoDB.DocumentClient();
+    if(req.body != null){
+        if(req.body.Username == null){
+            returnData(res, 0, null, '{Not all params present}');
+        }
+        else{
+            getUserByToken(res, req.headers, function(user){
+                if(user != undefined){
+                    console.log(user);
+                    getUser(user, function(data){
+                        if(data != null){
+                        var friends = data.Items[0].Friends;
+                        var found = false;
+                        for (var i in friends) {
+                            if(i == req.body.Username){
+                                found = true;
+                                break;
+                            }
+                        }                            
+                        if(found){
+                            console.log("removing");
+                            removeFriendFromUser(req.body.Username, user, function(succes){
+                                if(succes){
+                                    removeFriendFromUser(user, req.body.Username, function(succes){
+                                    if(succes){
+                                        var result = {};
+                                        returnData(res, 1, result, null);
+                                    }
+                                    else{
+                                        returnData(res, 0, null, '{RemoveItemError}');
+                                    }
+                                    });
+                                }
+                                else{
+                                    returnData(res, 0, null, '{RemoveItemError}');
                                 }
                             });
                         }
@@ -137,7 +212,7 @@ app.post('/friends/add',  function(req, res){
 
 });
     
-addFriendToUser = function(friend, user, callback){
+addFriendToUser = function(isSender, accepted, friend, user, callback){
     dd.updateItem({
         TableName: "Users",
         Key: {
@@ -154,7 +229,9 @@ addFriendToUser = function(friend, user, callback){
         ExpressionAttributeValues: {
             ":user": {
                 "M": {
-                    "DateAdded" : {"S" : new Date().toISOString()}
+                    "DateAdded" : {"S" : new Date().toISOString()},
+                    "IsSender" : {"BOOL" :  isSender},
+                    "Accepted" : {"BOOL" : accepted}                                  
                 }
             }
         }
@@ -165,6 +242,33 @@ addFriendToUser = function(friend, user, callback){
         }
         else
         {
+            callback(true);
+        }
+    });
+}
+
+removeFriendFromUser = function(friend, user, callback){
+    dd.updateItem({
+        TableName: "Users",
+        Key: {
+            "Username": {
+                "S": friend
+            }
+        },
+        UpdateExpression: "REMOVE #attrName.#attrName2",
+        //UpdateExpression : "SET #attrName = list_append(#attrName, :user)",
+        ExpressionAttributeNames : {
+        "#attrName" : "Friends",
+        "#attrName2" : user
+        }
+    }, function(err, data) {
+        if(err){
+            console.log(err);
+            callback(false)
+        }
+        else
+        {
+            console.log("ok");
             callback(true);
         }
     });
--- ServerCode/game.js
+++ ServerCode/game.js
@@ -3,10 +3,13 @@ exports = module.exports = function(app, AWS, dd){
 app.post('/game/create', function(req, res){
     var dynamodbDoc = new AWS.DynamoDB.DocumentClient();
     if(req.body != null){
-        if(req.body.Name == null ||req.body.MinPlayers == null || req.body.MaxPlayers == null ||req.body.MinPlayers >                       req.body.MaxPlayers){
+        console.log(req.body);
+        if(req.body.Name == null ||req.body.MinPlayers == null || req.body.MaxPlayers == null ||req.body.MinPlayers >                       req.body.MaxPlayers || req.body.CenterLocationLatitude == null || req.body.CenterLocationLongitude == null){
             returnData(res, 0, null, '{Not all params present}');
         }
         else{
+            console.log(getDistanceFromLatLonKmVincenty(req.body.CenterLocationLatitude, req.body.CenterLocationLongitude,
+                                                        req.body.CenterLocationLatitude+10, req.body.CenterLocationLongitude+10));
             user = getUserByToken(res, req.headers, function(user){
                 if(user != undefined){
                    var id = getTimeBaseUniqueId();
@@ -38,8 +41,8 @@ app.post('/game/join',  function(req, res){
                         var found = false;
                         var players = data.Items[0].Players;
                         for (var i in players) {
-                            value = players[i];
-                            if(value.Name == user){
+                            if(i == user){
+                                console.log("gelijk");
                                 found = true;
                                 break;
                             }
@@ -98,7 +101,7 @@ user = getUserByToken(res, req.headers, function(user){
                     item.MinPlayers = gamedata.MinPlayers;
                     item.MaxPlayers = gamedata.MaxPlayers;
                     item.Players = Object.keys(gamedata.Players);
-
+                    item.CenterLocation = gamedata.CircleCenter;
                     result.push(item);
                     
                 }  
@@ -108,20 +111,80 @@ user = getUserByToken(res, req.headers, function(user){
     }
 });
 });
+    
+    
+app.post('/game/sendData',  function(req, res){
+    var dynamodbDoc = new AWS.DynamoDB.DocumentClient();
+    if(req.body != null){
+        if(req.body.GameId == null || req.body.Accelerometer == null || req.body.Location == null){
+            returnData(res, 0, null, '{Not all params present}');
+        }
+        else{
+            user = getUserByToken(res, req.headers, function(user){
+                if(user != undefined){
+                getGame(req.body.GameId, function(data){
+                if (data == null) {
+                    console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+                }
+                else
+                {
+                    if(data.Items.length == 1){
+                        var found = false;
+                        var players = data.Items[0].Players;
+                        for (var i in players) {
+                            if(i == user){
+                                found = true;
+                                break;
+                            }
+                        }
+                        if(!found){
+                            var error = {'Errors' : [6]};
+                            returnData(res, 2, null, error);
+                        }  
+                        else
+                        {
+                            addDataToGame(req.body.GameId, user, req.body, function(succes){
+                                if(succes){
+                                    var result = {};
+                                    returnData(res, 1, result, null);
+                                }
+                                else{
+                                    returnData(res, 0, null, '{UpdateItemError}');
+                                }
+                            });
+                        }
+                    }
+                    else{
+                        var error = {'Errors' : [4]};
+                        returnData(res, 2, null, error);
+                    }
+                }
+                });
+                }
+            });
+        }
+    }
+});
                           
 
 putnewGameItem = function(data, username, id) {           
     
     var tableName = 'Games';
     var players = {"M" : {}};
-    players.M[username] = {"M":{"DateAdded" : {"S" : new Date().toISOString()} }}
+    players.M[username] = {"M":{"DateAdded" : {"S" : new Date().toISOString()} }};
     var item = {
 	    'GameId' : { 'S': id },
         'Name' : { 'S' : data.Name},
         'CreatedOn' : {'S' : new Date().toISOString()},
 	    'MinPlayers' : { 'N' : data.MinPlayers.toString()},
         'MaxPlayers' : { 'N' : data.MaxPlayers.toString()},
-        'Players' : players
+        'Players' : players,
+        'CircleCenter' : {
+                            "M" : {
+                                    Latitude:  {"N" : data.CenterLocationLatitude.toString()},
+                                    Longitude: {"N" : data.CenterLocationLongitude.toString()}        
+                                   }
+        }
     };	
     dd.putItem({
         'TableName': tableName,
@@ -149,8 +212,56 @@ addPlayerToGame = function(gameId, user, callback){
         ExpressionAttributeValues: {
             ":user": {
                 "M":{
-                    "DateAdded" : {"S" : new Date().toISOString()}
+                    "DateAdded" : {"S" : new Date().toISOString()},
+                    "Locations" : {"L" : []},
+                    "AccelerometerData" : {"L" : []}
                 }
+            }
+        }
+    }, function(err, data) {
+        if(err){
+            console.log(err);
+            callback(false)
+        }
+        else
+        {
+            callback(true);
+        }
+    });
+}
+
+addDataToGame = function(gameId, user, data, callback){
+    dd.updateItem({
+        TableName: "Games",
+        Key: {
+            "GameId": {
+                "S": gameId
+            }
+        },
+        //UpdateExpression: "ADD #attrName = :user",
+        //UpdateExpression : "SET #attrName = list_append(#attrName, :user)",
+        UpdateExpression: "SET #attrName.#attrName2.#attrName3 = list_append(#attrName.#attrName2.#attrName3, :location), #attrName.#attrName2.#attrName4 = list_append(#attrName.#attrName2.#attrName4, :accelerometer)",
+        ExpressionAttributeNames : {
+        "#attrName" : "Players",
+        "#attrName2" : user,
+        "#attrName3" : "Locations",
+        "#attrName4" : "AccelerometerData"
+        },
+        ExpressionAttributeValues: {
+            ":location": {
+               "L": [{"M":{
+                    "Longitude" : {"N" : data.Location.Longitude.toString()},
+                    "Latitude" : {"N": data.Location.Latitude.toString()}
+                }
+            }]
+            },
+            ":accelerometer": {
+               "L": [{"M":{
+                    "X" : {"N" : data.Accelerometer.X.toString()},
+                    "Y" : {"N": data.Accelerometer.Y.toString()},
+                    "Z" : {"N": data.Accelerometer.Z.toString()}
+                }
+            }]
             }
         }
     }, function(err, data) {
