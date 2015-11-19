f397422ab457fe05c32563aa233bb3f945b654a1
JeanFrerot <Jean@MacBook-Pro-van-Jean.local>
2015-11-19 16:08:44 +0100

    ni op letten, iets voor mezelf

---
+++ NightGame/app/src/main/java/be/cwa3/nightgame/ProximitysensorActivity.java
@@ -0,0 +1,7 @@
+package be.cwa3.nightgame;
+
+/**
+ * Created by Jean on 19/11/15.
+ */
+public class ProximitysensorActivity {
+}
---
+++ NightGame/app/src/main/java/be/cwa3/nightgame/Utils/SensorDataActivity.java
@@ -0,0 +1,162 @@
+package be.cwa3.nightgame.Utils;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import be.cwa3.nightgame.R;
+
+/**
+ * Created by jesse on 15/10/2015.
+ */
+public class SensorDataActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, SensorEventListener {
+    private boolean mRequestingLocationUpdates = true;
+    private Location mCurrentLocation;
+    private String mLastUpdateTime;
+    private GoogleApiClient mGoogleApiClient;
+    private SensorManager sensorManager;
+    LocationRequest mLocationRequest;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        createLocationRequest();
+        buildGoogleApiClient();
+        if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        // add listener. The listener will be  (this) class
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), sensorManager.SENSOR_DELAY_NORMAL);
+
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY),
+                sensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    public Location getLocation(){
+        return mCurrentLocation;
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(4000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        int type = event.sensor.getType();
+        if (type == Sensor.TYPE_ACCELEROMETER) {
+            // assign directions
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            Log.d("sensortest", String.valueOf(x));
+            Log.d("sensortest", String.valueOf(y));
+            Log.d("sensortest", String.valueOf(z));
+        }
+        else if(type==Sensor.TYPE_PROXIMITY){
+
+            float prox = event.values[0];
+
+            Log.d("sensortest", String.valueOf(prox));
+
+            // Use the construction below for compatibility on every device.
+            //if (event.values[0] == 0 ){
+            //
+            //}
+            //else{
+            //
+            //}
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
